1662830445518a54d66e35eecdb63c4f37230e99
Shakxtra - Milsim <50801784+shakxtra@users.noreply.github.com>
2025-08-29 12:44:35 -0600

    Delete HALs/Addons directory

--- HALs/Addons/money/config.hpp
+++
@@ -1,5 +0,0 @@
-class cfgHALsMoney {
-	debug = 0;
-	startingFunds = 50000; //Define dinero que recibe el player al conectar al servidor e iniciar la misión.
-	oldManItemsPrice[] = {50, 150, 300, 600}; //Toma la perspectiva de precios basado en arma APEX.
-};
